a08662f6883e98bf1114ca00e57bf04900864faa
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2023-06-09 22:47:48 +0530

    Create 2) Linked_List_Cycle.cpp

---
+++ Day 6: Linked List Part-II/2) Linked_List_Cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return NULL;
+
+        ListNode *s=head,*f = head;
+        while(f && f->next){
+            s=s->next;
+            f=f->next->next;
+            
+            if(s==f)
+                return true;
+            
+        }
+        return false;
+    }
+};
