b04585785e1c26fe9d2643ca7e60f1ec86b69e41
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2023-06-10 22:53:33 +0530

    Create 1) Rotate_List.cpp

---
+++ Day 7: Linked List and Arrays/1) Rotate_List.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || !k) return head;
+        int chk=1;
+        ListNode* temp=head;
+        
+        //finding length of ll
+        while(temp->next && ++chk)
+            temp=temp->next;
+        
+        //assigning next of last node to first
+        temp->next=head;
+        k%=chk;
+        k=chk-k;   
+        
+        //going to chk-k th node
+        while(k--)
+            temp=temp->next;
+        
+        head=temp->next;
+        temp->next=NULL;
+        return head;
+    }
+    
+};
