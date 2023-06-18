6ab3a9c499e55ee48985aad87a95bf4ea1fd1e3a
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2023-06-18 23:31:21 +0530

    Create Integer_to_Roman.cpp

---
+++ Day 15: String/3) Roman Number to Integer and vice versa/Integer_to_Roman.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<string> roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<int> numbers= {1000,900,500,400, 100,90, 50,40,10,9,5,4,1};
+        
+        string ans="";
+        
+        for(int i=0;i<roman.size();i++){ //traversing from high to low numbers
+            
+            while(num >= numbers[i]){
+                ans.append(roman[i]);
+                num-=numbers[i];
+            }
+        }
+        return ans;
+    }
+};
