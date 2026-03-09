8a9f73633711d4e492de8626b67c89cfcc4e359b
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2026-03-10 01:47:10 +0530

    Update 2) LRU Cache.cpp

--- Day 14: Stack and Queue Part-II/2) LRU Cache.cpp
+++ Day 14: Stack and Queue Part-II/2) LRU Cache.cpp
@@ -1,5 +1,84 @@
 class LRUCache {
 public:
+    class Node{
+        public:
+        Node* next,*prev;
+        int key, val;
+        Node(int _key,int _value){
+            key=_key;
+            val=_value;
+        }
+    };
+    Node* head=new Node(-1,-1);
+    Node* tail=new Node(-1,-1);
+
+    int cap;
+    unordered_map<int,Node*> mp;
+
+    LRUCache(int capacity) {
+        cap=capacity;
+        head->next=tail;
+        tail->prev=head;
+    }
+
+    void deleteNode(Node* node){
+        //delete location of node?
+        Node* prevNode=node->prev;
+        Node* nextNode=node->next;
+        prevNode->next=nextNode;
+        nextNode->prev=prevNode;
+    }
+
+    void insertNode(Node* node){
+        Node* temp=head->next;
+        head->next=node;
+        node->next=temp;
+        node->prev=head;
+        temp->prev=node;
+    }
+
+    int get(int key) {
+        if(mp.find(key) != mp.end()){
+            Node* node=mp[key];
+            int ans=node->val;
+            // mp.erase(key); // extra
+            deleteNode(node);
+            insertNode(node);
+            mp[key]=head->next;
+            return ans;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()){
+            Node* existingNode=mp[key];
+             mp.erase(key);  //we are removing it bcose in below in mp.size() we will be getting the wrong result as one is getting duplicated
+            deleteNode(existingNode);
+        }
+        if(mp.size()==cap){
+            mp.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+        Node* newNode= new Node(key,value);
+        insertNode(newNode);
+        mp[key]=head->next;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+
+
+---------------------------------------------------------------------------------------
+
+class LRUCache {
+public:
     list<pair<int,int>> l;
     int size;
     unordered_map<int,list<pair<int,int>>::iterator> mp;    //in map ...list<pair<int,int>>::iterator keeps the address of the  list l
