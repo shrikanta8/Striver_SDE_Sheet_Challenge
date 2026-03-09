ebc502a87c22c628c7d5037eca8f4ec2b04e5605
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2026-03-09 23:39:29 +0530

    Update 5) Check for Balanced Parentheses.cpp

--- Day 13: Stack and Queue/5) Check for Balanced Parentheses.cpp
+++ Day 13: Stack and Queue/5) Check for Balanced Parentheses.cpp
@@ -23,3 +23,18 @@ public:
         return true;
     }
 };
+
+
+
+Stack<Character> stack = new Stack<Character>();
+    for (char c : s.toCharArray()) {
+        if (c == '(')
+            stack.push(')');
+        else if (c == '{')
+            stack.push('}');
+        else if (c == '[')
+            stack.push(']');
+        else if (stack.isEmpty() || stack.pop() != c)
+            return false;
+    }
+    return stack.isEmpty();
