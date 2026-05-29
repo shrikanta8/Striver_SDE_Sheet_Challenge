97c3a830d50d6884465a8bc352dc614164f5b44b
Shrikanta Kalantri <42779746+shrikanta8@users.noreply.github.com>
2026-05-29 16:41:55 +0530

    Update 2) Minimum_Platforms.cpp

--- Day 8: Greedy Algorithm/2) Minimum_Platforms.cpp
+++ Day 8: Greedy Algorithm/2) Minimum_Platforms.cpp
@@ -1,30 +1,18 @@
-int findPlatform(int arr[], int dep[], int n)
-    {
+class Solution {
+  public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        // code here
+        int i=0,j=0,n=arr.size(),ans=0;
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
         
-        sort(arr, arr + n);
-        sort(dep, dep + n);
-    
-        // plat_needed indicates number of platforms
-        // needed at a time
-        int plat_needed = 1, result = 1;
-        int i = 1, j = 0;
-    
-        while (i < n) {
-            //arrival is less so we need a new platform for the train
-            if (arr[i] <= dep[j]) {
-                plat_needed++;
+        while(i<n){
+            while(i<n && arr[i]<=dep[j]){
                 i++;
             }
-            //if dep is less than arrival then it means that train is leaving so 
-            //one platform is cleared now!
-            else {
-                plat_needed--;
-                j++;
-            }
-    
-            if (plat_needed > result)
-                result = plat_needed;
+            ans=max(ans,i-j);
+            j++;
         }
-    
-        return result;
+        return ans;
     }
+};
